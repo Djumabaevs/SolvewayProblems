fe39550ec6951690d880b9bfac6c6efcd1d71494
Djumabaevs <djumabaevb@gmail.com>
2021-05-29 12:00:37 +0600

    1st attempt

--- src/problems/WordsMix.java
+++ src/problems/WordsMix.java
@@ -1,10 +1,29 @@
 package problems;
 
+
+
 public class WordsMix {
-    private String getDecodedMessage(int numberOfStrings, String encryptedMessage, int[]Array) {
+    private static String getDecodedMessage(int numberOfStrings, String encryptedMessage, int[]Array) {
+
+      char[] arrayOfChars = encryptedMessage.toCharArray();
+      int[] givenArray = Array;
+
+        for (int i = 0; i < arrayOfChars.length; i++) {
+            if(arrayOfChars[i] >= 'a' && arrayOfChars[i] <= 'z') {
+                char[] temp = arrayOfChars;
+                arrayOfChars[i] += givenArray[i];
+                System.out.println(arrayOfChars);
+
+            }
+        }
 
 
+      return arrayOfChars.toString();
+    }
 
-        return "decodedMessage";
+
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5};
+        getDecodedMessage(1, "hello", array);
     }
 }
