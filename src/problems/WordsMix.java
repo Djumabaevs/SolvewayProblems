2ecff0ff2f7d483fad3a95c4d857fd3235df04a9
Djumabaevs <djumabaevb@gmail.com>
2021-05-29 11:27:46 +0600

    create method

--- src/problems/WordsMix.java
+++ src/problems/WordsMix.java
@@ -1,5 +1,10 @@
 package problems;
 
 public class WordsMix {
-    
+    private String getDecodedMessage(int numberOfStrings, String encryptedMessage, int[]Array) {
+
+
+
+        return "decodedMessage";
+    }
 }
