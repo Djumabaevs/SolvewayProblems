d5b06bfaad69cff5f8f09c3712c54293f5d4a374
Djumabaevs <djumabaevb@gmail.com>
2021-05-30 14:25:22 +0600

    done

--- src/problems/OpenLock.java
+++ src/problems/OpenLock.java
@@ -10,19 +10,13 @@ public class OpenLock {
 
    static int openLock(int numberOfLocks, int[] numberOfCircles, int[] currentPosition, int[] targetPosition) {
 
-//       ArrayList<int[]> num = new ArrayList<>(numberOfLocks);
-//       for (int i = 0; i < numberOfLocks; i++) {
-//           num.add(numberOfCircles);
-//           num.add(currentPosition);
-//           num.add(targetPosition);
-//       }
 
-       if(numberOfCircles.length != numberOfLocks || currentPosition.length != numberOfLocks
-       || targetPosition.length != numberOfLocks) {
+       if (numberOfCircles.length != numberOfLocks || currentPosition.length != numberOfLocks
+               || targetPosition.length != numberOfLocks) {
            System.out.println("wrong arguments");
        }
 
-       if(Arrays.toString(currentPosition).equals(Arrays.toString(targetPosition))) {
+       if (Arrays.toString(currentPosition).equals(Arrays.toString(targetPosition))) {
            return 0;
        }
 
@@ -31,7 +25,7 @@ public class OpenLock {
        while (true) {
            for (int i = 0; i < copyOfArray.length; i++) {
                int item = copyOfArray[i];
-               if(item == numberOfCircles[i]) {
+               if (item == numberOfCircles[i]) {
                    copyOfArray[i] = 1;
                } else {
                    copyOfArray[i] = item + 1;
@@ -39,22 +33,25 @@ public class OpenLock {
            }
            step += 1;
 
-           if(Arrays.toString(copyOfArray).equals(Arrays.toString(currentPosition))) {
-               System.out.println("no solution");
+
+           if (Arrays.toString(copyOfArray).equals(Arrays.toString(currentPosition))) {
+               return -1;
            }
 
-           if(Arrays.toString(copyOfArray).equals(Arrays.toString(targetPosition))) {
+
+           if (Arrays.toString(copyOfArray).equals(Arrays.toString(targetPosition))) {
                return step;
            }
+
        }
 
    }
 
     public static void main (String[] args) {
-        int[] sizes = {4,4};
-        int[] current = {1,2};
-        int[] target = {1,3};
-        int result = openLock(2, sizes, current, target);
+        int[] sizes = {3,3,3,3};
+        int[] current = {1,1,1,1};
+        int[] target = {2,2,2,2};
+        int result = openLock(4, sizes, current, target);
         System.out.println(result);
     }
 
